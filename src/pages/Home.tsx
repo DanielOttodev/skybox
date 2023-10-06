a066711c15ebaf2fe18c116f257bc16ad294bebe
danielottodev <otto_281@hotmail.com>
2023-10-06 22:57:27 +1100

    logos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,30 @@
 import { useAuth } from "../hooks/useAuth"
+import { Paper } from "@mui/material";
+import Box from '@mui/material/Box';
 export default function Home(){
     const {onLogout} = useAuth();
 
     return(
         <div>
-            <h1>
-            Welcome to the Home page
-            </h1>
+    
+       
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > :not(style)': {
+          m: 1,
+          width: 128,
+          height: 128,
+        },
+      }}
+    >
+
+      <Paper elevation={3} />
+      <Paper elevation={3} />
+      <Paper elevation={3} />
+      <Paper elevation={1} />
+    </Box>
             <button onClick={() => { onLogout()}} className="p-5 bg-slate-300 m-5 border rounded hover:cursor-pointer">Sign Out</button>
         </div>
     )
